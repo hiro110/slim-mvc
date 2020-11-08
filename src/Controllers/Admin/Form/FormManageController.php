2d01f8549aa914b36a32c5e6f26f42e6cfb0a9bc
Hiroaki Ito <h.ito198701@gmail.com>
2020-11-09 06:06:36 +0900

    update form add page

--- src/Controllers/Admin/Form/FormManageController.php
+++ src/Controllers/Admin/Form/FormManageController.php
@@ -7,172 +7,203 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Container\ContainerInterface;
 use Slim\Exception\NotFoundException;
+use \Illuminate\Database\Capsule\Manager as DB;
+
 use App\Entities\Admin;
 use App\Daos\Admin\FormDAO;
 
-class FormManageController
+use App\Controllers\Controller;
+
+use App\Models\FormGroup;
+use App\Models\FormItem;
+
+class FormManageController extends Controller
 {
-		private $view;
-		private $db;
+	// 	private $view;
+	// 	private $db;
 
-	// コンストラクタ
-	public function __construct(ContainerInterface $container)
-	{
-				$this->view = $container->get("view");
-				$this->db = $container->get("db");
-	}
+	// // コンストラクタ
+	// public function __construct(ContainerInterface $container)
+	// {
+	// 			$this->view = $container->get("view");
+	// 			$this->db = $container->get("db");
+	// }
 
-		public function getforms(Request $request, Response $response, array $args): Response
-		{
-				try {
-						$formDao = new FormDAO($this->db);
-						$forms = $formDao->findAllForms();
+    public function getforms(Request $request, Response $response, array $args): Response
+    {
+        try {
+            $forms = FormGroup::where('is_active', 1)
+                    ->get();
 
-						if (!$forms) {
-								$msg = "Failed add form";
-						}
-				} catch(PDOException $ex) {
-						var_dump($ex->getMessage());
-				} finally {
-						// DB切断。
-						$this->db = null;
-				}
+            if (!$forms) {
+                $msg = "Failed add form";
+            }
+        } catch(PDOException $ex) {
+                var_dump($ex->getMessage());
+        }
 
-				$response = $this->view->render($response,
-											"admin/forms/index.html",
-										[
-											'forms' => $forms,
-										]);
+        $response = $this->view->render($response,
+                    "admin/forms/index.html",
+                    [
+                        'forms' => $forms,
+                    ]);
 
-				return $response;
-		}
+        return $response;
+    }
 
-		public function mapformsAdd(Request $request, Response $response, array $args): Response
-		{
+    public function mapformsAdd(Request $request, Response $response, array $args): Response
+    {
 
-				if ($request->getMethod() == "GET") {
-						$response = $this->view->render($response,
-													"admin/forms/edit.html",
-													['itemtypes' => FormDAO::ITEM_TYPE, 'valid_types' => FormDAO::VALIDATE_TYPE,]);
-						return $response;
-				}
+        if ($request->getMethod() == "GET") {
+            $response = $this->view->render($response,
+                        "admin/forms/edit.html",
+                        ['itemtypes' => FormItem::ITEM_TYPE, 'valid_types' => FormItem::VALIDATE_TYPE,]);
+            return $response;
+        }
+        $msg = "";
+        $params = $request->getParsedBody();
 
-				$params = $request->getParsedBody();
-				$form_group =[
-					'name' => isset($params["group_name"]) ? $params["group_name"]: '',
-					'base_uri' => isset($params["group_name"]) ? $params["group_name"]: '',
-				];
+        $name = isset($params["group_name"]) ? $params["group_name"]: '';
+        $base_uri = isset($params["group_uri"]) ? $params["group_uri"]: '';
+        $publishing_start = isset($params["publishing_start"]) ? date('Y-m-d H:i:s', strtotime($params["publishing_start"])): date("Y-m-d H:i:s");
+        $publishing_end = isset($params["publishing_end"]) ? date('Y-m-d H:i:s', strtotime($params["publishing_end"])): date("Y-m-d H:i:s");
 
-				$form_item =[
-					'label_name' => isset($params["label_name"]) ? $params["label_name"]: '',
-					'schema_name' => isset($params["schema_name"]) ? $params["schema_name"]: '',
-					'input_type' => isset($params["input_type"]) ? intVal($params["input_type"]): 0,
-					'placeholder' => isset($params["placeholder"]) ? $params["placeholder"]: '',
-					'is_required' => (intVal($params["is_required"]) == 1) ? 1: 0,
-					'choice_value' => isset($params["choice_value"]) ? $params["choice_value"]: '',
-					'validate' => isset($params["validate"]) ? $params["validate"]: '',
-				];
+        try {
+            // $con = DB::connection();
+            // $con->beginTransaction();
 
-				try {
-						$formDao = new FormDAO($this->db);
-						$forms = $formDao->addForm($form_group, $form_item);
+            $form_group = new FormGroup;
+            $form_group->name = $name;
+            $form_group->base_uri = $base_uri;
+            $form_group->is_active = 1;
+            $form_group->publishing_start = $publishing_start;
+            $form_group->publishing_end = $publishing_end;
+            $form_group->save();
+            $fid = $form_group->id;
 
-						if (!$forms) {
-								$msg = "Failed add form";
-						}
-				} catch(PDOException $ex) {
-						var_dump($ex->getMessage());
-				} finally {
-						// DB切断。
-						$this->db = null;
-				}
+            $form_items = [];
+            for($i = 0; $i < count($params["label_name"]); $i++) {
+                $form_items[] = [
+                    'label_name' => isset($params["label_name"][$i]) ? $params["label_name"][$i]: '',
+                    'schema_name' => isset($params["schema_name"][$i]) ? $params["schema_name"][$i]: '',
+                    'input_type' => isset($params["input_type"][$i]) ? $params["input_type"][$i]: '',
+                    'placeholder' => isset($params["placeholder"][$i]) ? $params["placeholder"][$i]: '',
+                    'is_required' => intVal($params["is_required"][$i]) == 1 ? 1: 0,
+                    'choice_value' => isset($params["choice_value"][$i]) ? $params["choice_value"][$i] : '',
+                    'validate' => isset($params["validate"][$i]) ? $params["validate"][$i]: '',
+                ];
+            }
 
-				$response = $this->view->render($response, "admin/forms/edit.html",['msg' => $msg]);
-				return $response;
+            foreach ($form_items as $form_item) {
+                $form_item = new FormItem;
+                $form_item->form_group_id = $fid;
+                $form_item->label_name = $form_item['label_name'];
+                $form_item->schema_name = $form_item['schema_name'];
+                $form_item->input_type = intVal($form_item['input_type']);
+                $form_item->placeholder = $form_item['placeholder'];
+                $form_item->is_required = intVal($form_item['is_required']);
+                $form_item->choice_value = $form_item['choice_value'];
+                $form_item->validate = $form_item['validate'];
+                $form_item->save();
+            }
 
-		}
+            // $con->commit();
+        } catch (\Exception $e) {
+            // $con->rollBack();
+            echo $e->getMessage();
+        }
 
-		public function mapformsId(Request $request, Response $response, array $args): Response
-		{
-				$fg_id = intVal($args['id']);
-				if ($request->getMethod() == "GET") {
-						$msg = "";
-						try {
-								$formDao = new FormDAO($this->db);
-								$group = $formDao->findGroupByPk($fg_id);
-								if (!$group) {
-									$msg = "Not found form";
-								}
+        $response = $this->view->render($response,
+                    "admin/forms/edit.html",
+                    [
+                        'msg' => $msg,
+                        'itemtypes' => FormItem::ITEM_TYPE,
+                        'valid_types' => FormItem::VALIDATE_TYPE,
+                    ]);
+        return $response;
+    }
 
-								$items = $formDao->findItemByGid($fg_id);
-								if (!$items) {
-									$msg = "Not found form items";
-								}
+    public function mapformsId(Request $request, Response $response, array $args): Response
+    {
+            $fg_id = intVal($args['id']);
+            if ($request->getMethod() == "GET") {
+                    $msg = "";
+                    try {
+                            $formDao = new FormDAO($this->db);
+                            $group = $formDao->findGroupByPk($fg_id);
+                            if (!$group) {
+                                $msg = "Not found form";
+                            }
 
-						} catch(PDOException $ex) {
-								var_dump($ex->getMessage());
-						} finally {
-								// DB切断。
-								$this->db = null;
-						}
+                            $items = $formDao->findItemByGid($fg_id);
+                            if (!$items) {
+                                $msg = "Not found form items";
+                            }
 
-						$response = $this->view->render($response,
-													"admin/forms/edit.html",
-													[
-															'form_group' => $group,
-															'form_items' => $items,
-															'msg' => $msg,
-															'itemtypes' => FormDAO::ITEM_TYPE,
-															'valid_types' => FormDAO::VALIDATE_TYPE,
-													]);
-						return $response;
-				}
+                    } catch(PDOException $ex) {
+                            var_dump($ex->getMessage());
+                    } finally {
+                            // DB切断。
+                            $this->db = null;
+                    }
 
-				$params = $request->getParsedBody();
-				$form_group =[
-					'id' => $fg_id,
-					'name' => isset($params["group_name"]) ? $params["group_name"]: '',
-					'base_uri' => isset($params["group_name"]) ? $params["group_name"]: '',
-				];
+                    $response = $this->view->render($response,
+                                                "admin/forms/edit.html",
+                                                [
+                                                        'form_group' => $group,
+                                                        'form_items' => $items,
+                                                        'msg' => $msg,
+                                                        'itemtypes' => FormDAO::ITEM_TYPE,
+                                                        'valid_types' => FormDAO::VALIDATE_TYPE,
+                                                ]);
+                    return $response;
+            }
 
-				$form_items = [];
-				for($i = 0; $i < count($params["label_name"]); $i++) {
-					$form_items[] = [
-						'label_name' => isset($params["label_name"][$i]) ? $params["label_name"][$i]: '',
-						'schema_name' => isset($params["schema_name"][$i]) ? $params["schema_name"][$i]: '',
-						'input_type' => isset($params["input_type"][$i]) ? $params["input_type"][$i]: '',
-						'placeholder' => isset($params["placeholder"][$i]) ? $params["placeholder"][$i]: '',
-						'is_required' => (intVal($params["is_required"][$i]) == 1) ? 1: 0,
-						'choice_value' => isset($params["choice_value"][$i]) ? $params["choice_value"][$i] : '',
-						'validate' => isset($params["validate"][$i]) ? $params["validate"][$i]: '',
-					];
-				}
+            $params = $request->getParsedBody();
+            $form_group =[
+                'id' => $fg_id,
+                'name' => isset($params["group_name"]) ? $params["group_name"]: '',
+                'base_uri' => isset($params["group_name"]) ? $params["group_name"]: '',
+            ];
 
-				try {
-						$formDao = new FormDAO($this->db);
-						$res = $formDao->updateForm($form_group, $form_items);
+            $form_items = [];
+            for($i = 0; $i < count($params["label_name"]); $i++) {
+                $form_items[] = [
+                    'label_name' => isset($params["label_name"][$i]) ? $params["label_name"][$i]: '',
+                    'schema_name' => isset($params["schema_name"][$i]) ? $params["schema_name"][$i]: '',
+                    'input_type' => isset($params["input_type"][$i]) ? $params["input_type"][$i]: '',
+                    'placeholder' => isset($params["placeholder"][$i]) ? $params["placeholder"][$i]: '',
+                    'is_required' => (intVal($params["is_required"][$i]) == 1) ? 1: 0,
+                    'choice_value' => isset($params["choice_value"][$i]) ? $params["choice_value"][$i] : '',
+                    'validate' => isset($params["validate"][$i]) ? $params["validate"][$i]: '',
+                ];
+            }
 
-						if (!$res) {
-								$msg = "Failed update form";
-						}
-				} catch(PDOException $ex) {
-						var_dump($ex->getMessage());
-				} finally {
-						// DB切断。
-						$this->db = null;
-				}
+            try {
+                    $formDao = new FormDAO($this->db);
+                    $res = $formDao->updateForm($form_group, $form_items);
 
-				return $response->withHeader('Location', '/admin/forms')->withStatus(302);
+                    if (!$res) {
+                            $msg = "Failed update form";
+                    }
+            } catch(PDOException $ex) {
+                    var_dump($ex->getMessage());
+            } finally {
+                    // DB切断。
+                    $this->db = null;
+            }
 
-				// $response = $this->view->render($response, "admin/forms/edit.html",
-				// 						[
-				// 							'user' => [
-				// 								'username' => $username,
-				// 								'role' => $role,
-				// 							],
-				// 							'msg' => $msg,
-				// 							'roles' => UserDAO::ROLES
-				// 						]);
-				// return $response;
-		}
+            return $response->withHeader('Location', '/admin/forms')->withStatus(302);
+
+            // $response = $this->view->render($response, "admin/forms/edit.html",
+            // 						[
+            // 							'user' => [
+            // 								'username' => $username,
+            // 								'role' => $role,
+            // 							],
+            // 							'msg' => $msg,
+            // 							'roles' => UserDAO::ROLES
+            // 						]);
+            // return $response;
+    }
 }
